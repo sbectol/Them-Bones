039904ba2333a0c91857a0f342e5f5a5e38b3947
SImon Beech <simon.beech@gmail.com>
2022-06-23 13:09:11 +0100

    found objects

--- Assets/Scripts/TextMover.cs
+++ Assets/Scripts/TextMover.cs
@@ -12,16 +12,21 @@ public class TextMover : MonoBehaviour
     private GameObject textObject1;
     private GameObject textObject2;
     private GameObject textObject3;
+    private GameObject frame;
     private GameObject tooth;
     private GameObject mainCamera;
-    private CanvasGroup[] parts = new CanvasGroup[14];
+    private CanvasGroup catBluePrint;
+    private CanvasGroup[] parts = new CanvasGroup[15];
     private Vector2[] coordsCB = new Vector2[13];
+    private Vector2[] coordsCresswell = new Vector2[15];
     private Transform endMarker1;
     private Transform startMarker1;
     private float speed;
     private float latitude;
     private float longitude;
     private int journeyProgress;
+    private int closest;
+    private float minDistance = 1000;
     void Start()
     {
         Input.compass.enabled = true;
@@ -29,6 +34,23 @@ public class TextMover : MonoBehaviour
         {
             StartCoroutine(GetLocation());
         }
+
+        coordsCresswell[1] = new Vector2(53.26301182857125f, -1.193743944168091f);
+        coordsCresswell[2] = new Vector2(53.26306958489932f, -1.1971020698547366f);
+        coordsCresswell[3] = new Vector2(53.262569027464444f, -1.1982500553131106f);
+        coordsCresswell[4] = new Vector2(53.26229307662967f, -1.198915243148804f);
+        coordsCresswell[5] = new Vector2(53.26197861885664f, -1.2005996704101565f);
+        coordsCresswell[6] = new Vector2(53.26175400474593f, -1.2015652656555178f);
+        coordsCresswell[7] = new Vector2(53.261503719061544f, -1.20275616645813f);
+        coordsCresswell[8] = new Vector2(53.26099672562242f, -1.202058792114258f);
+        coordsCresswell[9] = new Vector2(53.26121492606713f, -1.2010395526885989f);
+        coordsCresswell[10] = new Vector2(53.261574312608026f, -1.2000739574432375f);
+        coordsCresswell[11] = new Vector2(53.2618053452181f, -1.19876503944397f);
+        coordsCresswell[12] = new Vector2(53.26202995905914f, -1.19776725769043f);
+        coordsCresswell[13] = new Vector2(53.26222248426966f, -1.1971557140350342f);
+        coordsCresswell[14] = new Vector2(53.26242784353887f, -1.1964797973632812f);
+                                                                                                                                                           
+
         coordsCB[1] = new Vector2(53.291048960315216f, -3.7248343819867595f);  //Home
         coordsCB[2] = new Vector2(53.29135f, -3.71566f);  //Corner of Leisure Centre
         coordsCB[3] = new Vector2(53.29301f, -3.71410f);  //Stone Circle
@@ -43,6 +65,7 @@ public class TextMover : MonoBehaviour
         coordsCB[10] = new Vector2(53.297953888903734f, -3.727067838982684f);  //Colwyn Sign
         coordsCB[11] = new Vector2(53.29979923676437f, -3.7315745163052583f);  // The Toad
         coordsCB[12] = new Vector2(53.29641334528825f, -3.7318060268120283f);   //War Memorial
+        frame = GameObject.Find("frame");
         textObject1 = GameObject.Find("Target1");
         textObject2= GameObject.Find("Target2");
         textObject3 = GameObject.Find("Target3");
@@ -53,7 +76,8 @@ public class TextMover : MonoBehaviour
         startMarker1 = mainCamera.GetComponent<Transform>();
         endMarker1 = startMarker1;
         speed = 25f;
-
+        catBluePrint = GameObject.Find("CatBluePrint").GetComponent<CanvasGroup>();
+        catBluePrint.alpha = 0;
         parts[1] = GameObject.Find("Part1").GetComponent<CanvasGroup>();
         parts[2] = GameObject.Find("Part2").GetComponent<CanvasGroup>();
         parts[3] = GameObject.Find("Part3").GetComponent<CanvasGroup>();
@@ -70,7 +94,7 @@ public class TextMover : MonoBehaviour
 
         
 
-        for (int i = 1; i < 13; i++)
+        for (int i = 1; i < 14; i++)
         {
             parts[i].alpha = 0;
         }
@@ -110,7 +134,28 @@ public class TextMover : MonoBehaviour
     {
         latitude = Input.location.lastData.latitude;
         longitude = Input.location.lastData.longitude;
-        Debug.Log(Distance(latitude, longitude, coordsCB[1].x, coordsCB[1].y));
+        //Debug.Log(Distance(latitude, longitude, coordsCB[1].x, coordsCB[1].y));
+        //find closest
+        
+        for (int i = 1; i < 15; i++)
+        {
+            
+        if(Distance(latitude, longitude, coordsCresswell[i].x, coordsCresswell[i].y) <= minDistance)
+            {
+                closest = i;
+                minDistance = Distance(latitude, longitude, coordsCresswell[i].x, coordsCresswell[i].y);
+              
+
+            }
+        }
+        Debug.Log("The closest is " + closest.ToString());
+        switch (journeyProgress)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
 
         mainCamera.transform.Rotate(Vector3.forward, 10.0f * Time.deltaTime);
         mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, endMarker1.position, speed * Time.deltaTime);
